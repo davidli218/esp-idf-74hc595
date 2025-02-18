8b50a149e6c36a2b8940b63bfa28086b60752e8b
Ri Teii <teiiri@outlook.com>
2025-02-18 18:35:36 +0000

    🔄 Reorder members in `x4hc595_config_t`

--- include/x4hc595.h
+++ include/x4hc595.h
@@ -9,11 +9,11 @@
  * @brief Configuration structure for x4HC595 shift register.
  */
 typedef struct {
-    gpio_num_t mr_;     /*!< Master Reset */
+    gpio_num_t ds;      /*!< Data Serial */
     gpio_num_t shcp;    /*!< Shift Register Clock Pulse */
     gpio_num_t stcp;    /*!< Storage Register Clock Pulse */
+    gpio_num_t mr_;     /*!< Master Reset */
     gpio_num_t oe_;     /*!< Output Enable */
-    gpio_num_t ds;      /*!< Data Serial */
     size_t num_devices; /*!< Number of cascaded devices */
 } x4hc595_config_t;
 
